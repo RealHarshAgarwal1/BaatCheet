2dada60eacd395062591dca9f6f9dd1c47bcc78d
Realharshagarwal1 <realharshagarwal@gmail.com>
2026-02-05 23:55:27 +0530

    Setup Redis connection

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js"
+import {createClient} from "redis";
 
 dotenv.config();
 
+// Verify REDIS_URL is loaded
+if (!process.env.REDIS_URL) {
+    throw new Error("REDIS_URL environment variable is not defined. Check your .env file.");
+}
+
 connectDb();
+
+export const redisClient = createClient(
+    {
+        url: process.env.REDIS_URL,
+    }
+);
+
+redisClient.connect().then(()=>console.log("Connected to Redis"));
 const app = express();
 
 const port =process.env.PORT;
